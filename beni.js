e49fb7cac75a3db113fd0620bbcb974c68f215c3
benidama <benidama02@gmail.com>
2022-06-02 10:17:52 +0200

    woo commit

---
+++ beni.js
@@ -0,0 +1,54 @@
+//const varia = "john";
+//const age = 34;
+
+//console.log("hello my name is " + varia + " and my age is " + age + " ,I love you all!");
+//console.log("hello my name is ${varia} and my age is  ${age}  ,I love you all!");
+
+
+//let myname = {
+   // name: "john",
+    //age: 56,
+    //mydoge: "peter",
+//}
+//this
+//console.log(myname.age);
+
+
+//let text = document.querySelector(".text");
+//let change = document.querySelector(".changecolor");
+
+//text.style.textTransform = "upperCase";
+
+//document.getElementById("all").backgroundColor = "blue";
+
+// failed
+//document.getElementById("all").style.backgroundColor = "aqua";
+//document.addEventListener("click", function(){
+    //this.textContent.classlist.add("text");
+//});
+
+//const usercome = document.querySelectorAll(".list");
+//const listput =  document.querySelector(".incat");
+//const addlitt = document.querySelector(".add")
+//for (user of usercome) {
+    //user.addEventListener("click", function(){
+       // this.style.color = "green";
+    //});
+//}
+//console.log(listput.value);
+
+
+function addnumber(){
+    for(a > 0, a<=100, a++){
+        if(a % 3&& 5 === 0){
+            return "fizzbuz";
+        } else if (a % 3 ===0){
+            return "fizz";
+        } else if (a % === 0){
+            return "buz";
+        } else {
+            return "unknown";
+        }
+    }
+}
+addnumber();
